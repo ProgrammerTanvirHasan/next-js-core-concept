3e36f4c5b3fb4c65c6f70faa25d88de81088d350
Md.Tanvir <hasanmdtanvir916@gmail.com>
2026-01-12 23:52:45 +0600

    next-js full app initialize

---
+++ src/note.ts
@@ -0,0 +1,3 @@
+// Turbopack is a Rust-based bundler(translator) for Next.js that significantly improves development speed by rebuilding only changed files instead of the entire app.
+
+// Webpack সব file আবার process করে, Turbopack শুধু যেটা বদলেছে সেটাই করে। joto rokomer file ace jmn js,ts,css,import,export shob kicu ke bundle kore browser ke supply kore.Because Browser এসব আলাদা আলাদা বুঝে না
