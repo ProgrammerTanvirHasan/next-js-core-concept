ea4c7290369124b19d66ff207a0955ef0e8f5725
Md.Tanvir <hasanmdtanvir916@gmail.com>
2026-01-21 00:26:33 +0600

    send dynamic params with config

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -2,8 +2,17 @@ import { Button } from "@/components/ui/button";
 import { blogService } from "@/services/blog.service";
 
 export default async function Home() {
-  const { data } = await blogService.getBlogs();
-  console.log(data);
+  const { data } = await blogService.getBlogs(
+    {
+      isFeatured: true,
+      search: "",
+    },
+    {
+      cache: "no-store", //or
+      revalidate: 10, //0r
+    },
+  );
+
   return (
     <div>
       <Button variant="outline">Click here</Button>
--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -2,10 +2,41 @@ import { env } from "@/env";
 
 const API_URL = env.AUTH_URL;
 
+interface getBlogsParams {
+  isFeatured?: boolean;
+  search?: string;
+}
+
+interface ServiceOption {
+  cache?: RequestCache;
+  revalidate?: number;
+}
+
 export const blogService = {
-  getBlogs: async function () {
+  getBlogs: async function (params?: getBlogsParams, options?: ServiceOption) {
     try {
-      const res = await fetch(`${API_URL}/posts`);
+      const url = new URL(`${API_URL}/posts`);
+      //   url.searchParams.append("key", "value"); //url er moddhe chayle dynamic kicu add korte pari
+
+      if (params) {
+        //same vabe ekhanew params ta get korte pari
+        Object.entries(params).forEach(([key, value]) => {
+          if (value !== undefined && value !== null && value !== "") {
+            url.searchParams.append("key", "value");
+          }
+        });
+      }
+
+      const config: RequestInit = {};
+      if (options?.cache) {
+        config.cache = options.cache;
+      }
+      if (options?.revalidate) {
+        config.next = { revalidate: options.revalidate };
+      }
+
+      const res = await fetch(url.toString(), config); //fetch er moddhe must ekta plan text string pathate hobe er jonnoi toString use kora hoyece
+
       const session = await res.json();
 
       return { data: session, error: null };
@@ -14,3 +45,5 @@ export const blogService = {
     }
   },
 };
+
+//ei khane muloto kaj ta hocce : jokhon amra url a dynamic params add korte chay,fetch korar shomoi jokhon dynaic params pathate chay tokhon prothome Object.entries theke params ta dhorlam then url.searchParams.append kore key,value ta url a set kore dilam r fetch er moddhe url ta pathiye dilam
